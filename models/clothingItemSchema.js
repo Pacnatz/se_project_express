9ad867a794cad28bc2306e98f4a8f2e765f7494a
Pacnatz <Nhatchu04@yahoo.com>
2026-01-20 00:31:02 -0600

    Add schemas

---
+++ models/clothingItemSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Item name is required"],
+    minlength: 2,
+    maxLength: 30,
+  },
+  weather: {
+    type: String,
+    required: [true, "Weather URL is required"],
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Image URL is required"],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must provide a valid URL for the image.",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: [true, "Owner is required"],
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothingItem", clothingItemSchema);
---
+++ models/userSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    minlength: 2,
+    maxLength: 30,
+  },
+  avatar: {
+    type: String,
+    required: [true, "Avatar URL is required"],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must provide a valid URL for the avatar.",
+    },
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
